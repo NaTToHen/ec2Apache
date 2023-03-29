a36230e3fbf17667a83dff32b477d6b4d33ec0a9
nattohen <henriquebonatto008@hotmail.com>
2023-03-29 18:46:28 +0000

    adicionando o script de validacao

---
+++ validacao.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+data=$(date +"%d-%m-%Y %H:%M:%S")
+
+# Verifica
+if systemctl is-active httpd >/dev/null 2>&1; then
+  mensagem="o serviço Apache está online."
+  arquivo="online.txt"
+else
+  mensagem="o serviço Apache está offline."
+  arquivo="offline.txt"
+fi
+
+echo "No momento $data, $mensagem" >> Henrique/$arquivo
